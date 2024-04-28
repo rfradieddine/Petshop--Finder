08bc75922568705123aa42708e8c365adb3224b3
Rafael Radieddine <rafaelradieddine@gmail.com>
2024-04-28 20:55:07 -0300

    make it better

--- apipetshopfinder/knexfile.js
+++ apipetshopfinder/knexfile.js
@@ -21,18 +21,21 @@ module.exports = {
   },
 
   production: {
-    client: 'postgresql',
-    connection: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
       database: 'my_db',
       user:     'username',
       password: 'password'
-    },
+    ,
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
-      tableName: 'knex_migrations'
+      directory: __dirname + '/database/migrations'
+    },
+    seeds: {
+      directory: __dirname + '/database/seeds'
     }
   }
 
