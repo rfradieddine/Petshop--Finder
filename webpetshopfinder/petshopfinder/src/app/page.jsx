9eb13d6e4d84bfd243bb15037af5588705dc15a5
Rafael Radieddine <rafaelradieddine@gmail.com>
2024-04-29 00:24:55 -0300

    Revert "Delete webpetshopfinder/petshopfinder directory"
    This reverts commit 4553d56b702004e432f48109884baaa4f20b2d9a.

---
+++ webpetshopfinder/petshopfinder/src/app/page.jsx
@@ -0,0 +1,31 @@
+'use client'
+
+import Image from "next/image";
+import { Box, Typography, useMediaQuery, useTheme } from "@mui/material";
+import background from "../../public/dog_logo4.png"
+import '@fontsource/roboto/700.css';
+import TextFieldsCard from "../components/TextFieldCard";
+
+export default function Home() {
+  const theme = useTheme();
+  const isSmallerScreen = useMediaQuery(theme.breakpoints.down('sm'));
+
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100vh",
+        gap: theme.spacing(0.1),
+      }}
+    >
+      
+      <Image src={background} alt="dog" width={isSmallerScreen ? 200 : 400} height={isSmallerScreen ? 200 : 400} />
+      <Typography variant={isSmallerScreen ? "h3" : "h2"} color="white" style={{ fontFamily: 'Pacifico', fontWeight: 500, letterSpacing: 2, marginBottom: 10, marginTop: -50}}>PetShopFinder</Typography>
+      <Typography align="center">O aplicativo para buscar os melhores preços para seus pets!</Typography>
+      <TextFieldsCard />
+    </Box>
+  );
+}
